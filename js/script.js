1fb298955022a17fc11e3e8e7d3aa58ec1de3088
LucaRiccio <lucariccio888@gmail.com>
2020-07-13 15:30:05 +0200

    Array + Prompt + Push + Upper/Lower/Slice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert("ciao");
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+var cognomi = ["Bianchi","Rossi","Duzioni","Balsano","Verdi"];
+var cognomeUtente = prompt("Inserisci il tuo cognome");
+var parola = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1).toLowerCase();
+cognomi.push (parola);
+console.log(cognomi);
+
+// var i = 0;
+// while (i<cognomi.lenght) {
+//   i++;
+//
+//
+// }
