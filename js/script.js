742d792c54278108ba4daf830006d48a31e70c6b
LucaRiccio <lucariccio888@gmail.com>
2020-07-13 16:12:35 +0200

    Aggiunta Stampa Lista Ordinata

--- js/script.js
+++ js/script.js
@@ -7,11 +7,9 @@ var cognomi = ["Bianchi","Rossi","Duzioni","Balsano","Verdi"];
 var cognomeUtente = prompt("Inserisci il tuo cognome");
 var parola = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1).toLowerCase();
 cognomi.push (parola);
+cognomi.sort();
 console.log(cognomi);
 
-// var i = 0;
-// while (i<cognomi.lenght) {
-//   i++;
-//
-//
-// }
+for (var i = 0; i < cognomi.length; i++) {
+  document.getElementById('lista').innerHTML += "<li>" + cognomi[i] + "</li>";
+}
